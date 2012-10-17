a765fae07c3643e060f98377a096241fbab28175
jcserrano <jorgecamse@gmail.com>
2012-10-18 00:40:24 +0200

    Initial commit

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,44 @@
+class MoviesController < ApplicationController
+  def show
+    @movie = Movie.find(params[:id])
+
+  end
+
+  def index
+    @movies = Movie.all
+
+  end
+
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+    @movie = Movie.create!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(params[:movie])
+    respond_to do |client_wants|
+      client_wants.html do
+        flash[:notice] = "#{@movie.title} was successfully updated."
+        redirect_to movie_path(@movie)  
+      end
+      client_wants.xml  {  render :xml => @movie.to_xml    }
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = '2f4d119d8b2588109447726da6bfbfead7455ca0081da7a1515d92a181c02163cb78de6fb2ce5a92ba133cb994866ccca77918d2004577283a8afbf188260f09'
